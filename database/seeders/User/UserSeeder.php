4aaa7b47282befc08ed009fd578b0e409a2aee64
Muhammad Yunus <muhammadyunus1072@gmail.com>
2026-03-30 10:54:38 +0700

    user

--- database/seeders/User/UserSeeder.php
+++ database/seeders/User/UserSeeder.php
@@ -18,67 +18,72 @@ class UserSeeder extends Seeder
             'name' => "Admin",
             'username' => "admin",
             'email' => "admin@gmail.com",
-            'password' => Hash::make("123"),
+            'password' => Hash::make("h5qlVDPMXL9OYQ1NYl71"),
         ]);
 
         $user->assignRole('Super Admin');
 
 
+        // PAK NOVI
         $user = User::create([
             'name' => "Pak Novi",
             'username' => "Pak Novi",
             'email' => "pak_novi@gmail.com",
-            'password' => Hash::make("123"),
+            'password' => Hash::make("123exata"),
         ]);
 
         $user->assignRole('Pak Novi');
 
 
+        // ACC EXATA
         $user = User::create([
-            'name' => "Acc Exata",
-            'username' => "Acc Exata",
-            'email' => "acc_exata@gmail.com",
-            'password' => Hash::make("123"),
+            'name' => "Nurul - Acc Exata",
+            'username' => "Nurul",
+            'email' => "nurul.exataindonesia2018@gmail.com",
+            'password' => Hash::make("123exata"),
         ]);
 
         $user->assignRole('Acc Exata');
 
 
+        // HS
         $user = User::create([
             'name' => "HS",
             'username' => "HS",
             'email' => "hs@gmail.com",
-            'password' => Hash::make("123"),
+            'password' => Hash::make("123exata"),
         ]);
 
         $user->assignRole('HS');
 
 
+        // CONTENT CREATOR
         $user = User::create([
-            'name' => "CC",
-            'username' => "CC",
-            'email' => "cc@gmail.com",
-            'password' => Hash::make("123"),
+            'name' => "Teddy",
+            'username' => "Teddy - Content Creator",
+            'email' => "teddy.exata@gmail.com",
+            'password' => Hash::make("123exata"),
         ]);
 
         $user->assignRole('CC');
 
 
+        // FINANCE
         $user = User::create([
-            'name' => "Finance",
-            'username' => "Finance",
-            'email' => "finance@gmail.com",
-            'password' => Hash::make("123"),
+            'name' => "Rina",
+            'username' => "Rina - Finance",
+            'email' => "rinaexataindonesia@gmail.com",
+            'password' => Hash::make("123exata"),
         ]);
 
         $user->assignRole('Finance');
 
-
+        // SALES
         $user = User::create([
-            'name' => "Sales",
-            'username' => "Sales",
-            'email' => "sales@gmail.com",
-            'password' => Hash::make("123"),
+            'name' => "Mukhamad Turhamun",
+            'username' => "Mukhamad Turhamun",
+            'email' => "kim.exataindonesia2018@gmail.com",
+            'password' => Hash::make("123exata"),
         ]);
 
         $user->assignRole('Sales');
